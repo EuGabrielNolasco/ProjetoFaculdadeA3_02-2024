9dfaa7ba9a410403b03b07a6ea6d744ee096a50d
Gabriel <nexkho2003@gmail.com>
2024-11-04 22:52:50 -0300

    elaborando tela de home do administrador

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,105 +2,66 @@
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900">
-        Bem-vindo à sua aplicação Jetstream!
+        Bem-vindo ao sistema de gestão de escalas e turnos!
     </h1>
 
     <p class="mt-6 text-gray-500 leading-relaxed">
-        O Laravel Jetstream fornece um ponto de partida bonito e robusto para sua próxima aplicação Laravel. O Laravel foi projetado
-        para ajudá-lo a construir sua aplicação usando um ambiente de desenvolvimento que é simples, poderoso e agradável. Acreditamos
-        que você deve amar expressar sua criatividade por meio da programação, então gastamos tempo cuidadosamente elaborando o ecossistema Laravel
-        para ser uma lufada de ar fresco. Esperamos que você adore.
+        Este projeto foi desenvolvido para atender a necessidade de um estabelecimento em organizar e distribuir os horários de trabalho de seus funcionários de maneira prática e acessível. Através deste sistema, o usuário poderá visualizar sua escala semanal ou mensal, além de ter acesso à escala de todos os demais colaboradores sem precisar recorrer a sistemas externos ou ao uso excessivo de papel.
+    </p>
+
+    <p class="mt-6 text-gray-500 leading-relaxed">
+        A plataforma oferece uma área dedicada para o administrador, onde é possível gerenciar escalas, cadastrar turnos, cargos, departamentos e horários, além de gerar relatórios e registrar novos usuários. Todo o gerenciamento de informações foi pensado para reduzir o uso de papéis, oferecendo uma experiência dinâmica e intuitiva para o administrador e funcionários.
+    </p>
+
+    <p class="mt-6 text-gray-500 leading-relaxed">
+        Com este sistema, a tradicional prática de pendurar papéis nas paredes pode ser eliminada, oferecendo uma alternativa digital onde cada funcionário consegue acessar facilmente sua escala. Nas tabelas, os usuários podem pesquisar por nome, cargo e outras informações relevantes, tornando o acesso rápido e eficiente.
+    </p>
+
+    <p class="mt-6 text-sm">
+        <a href="https://github.com/eugabrielnolasco/ProjetoFaculdadeA3_02-2024" target="_blank" class="inline-flex items-center font-semibold text-indigo-700">
+            Acesse o repositório no GitHub
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500">
+                <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
+            </svg>
+        </a>
     </p>
 </div>
 
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
     <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
-            </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900">
-                <a href="https://laravel.com/docs">Documentação</a>
-            </h2>
-        </div>
-
+        <h2 class="ms-3 text-xl font-semibold text-gray-900">
+            Organização de Escalas
+        </h2>
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            O Laravel possui uma documentação maravilhosa cobrindo todos os aspectos do framework. Se você é novo no framework ou tem experiência anterior, recomendamos que você leia toda a documentação do início ao fim.
-        </p>
-
-        <p class="mt-4 text-sm">
-            <a href="https://laravel.com/docs" class="inline-flex items-center font-semibold text-indigo-700">
-                Explore a documentação
-
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500">
-                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
-                </svg>
-            </a>
+            O sistema permite que o administrador crie e distribua escalas de forma eficiente, visualizando e ajustando turnos e horários conforme necessário para garantir a organização do estabelecimento.
         </p>
     </div>
 
     <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
-            </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900">
-                <a href="https://laracasts.com">Laracasts</a>
-            </h2>
-        </div>
-
+        <h2 class="ms-3 text-xl font-semibold text-gray-900">
+            Cargos e Departamentos
+        </h2>
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            O Laracasts oferece milhares de tutoriais em vídeo sobre Laravel, PHP e desenvolvimento JavaScript. Confira, veja por si mesmo e melhore suas habilidades de desenvolvimento nesse processo.
-        </p>
-
-        <p class="mt-4 text-sm">
-            <a href="https://laracasts.com" class="inline-flex items-center font-semibold text-indigo-700">
-                Comece a assistir Laracasts
-
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500">
-                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
-                </svg>
-            </a>
+            Além das escalas, é possível gerenciar os cargos e departamentos dos funcionários, permitindo um controle detalhado sobre a estrutura organizacional da empresa.
         </p>
     </div>
 
     <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900">
-                <a href="https://tailwindcss.com/">Tailwind</a>
-            </h2>
-        </div>
-
+        <h2 class="ms-3 text-xl font-semibold text-gray-900">
+            Relatórios e Análises
+        </h2>
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            O Laravel Jetstream é construído com o Tailwind, uma incrível framework CSS utilitário que não atrapalha seu trabalho. Você ficará surpreso com a facilidade de construir e manter designs modernos e frescos com este maravilhoso framework ao seu alcance.
+            O sistema facilita a geração de relatórios, permitindo ao administrador visualizar dados sobre as escalas, turnos e outros registros importantes para uma análise detalhada do desempenho e da organização.
         </p>
     </div>
 
     <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-            </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900">
-                <a href="https://github.com/laravel/laravel">Laravel no GitHub</a>
-            </h2>
-        </div>
-
+        <h2 class="ms-3 text-xl font-semibold text-gray-900">
+            Redução de Papel
+        </h2>
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            O código-fonte do Laravel é hospedado no GitHub. Confira o repositório para ver exemplos de como a comunidade Laravel está ajudando a melhorar o framework.
-        </p>
-
-        <p class="mt-4 text-sm">
-            <a href="https://github.com/laravel/laravel" class="inline-flex items-center font-semibold text-indigo-700">
-                Veja o código no GitHub
-
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500">
-                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
-                </svg>
-            </a>
+            Este projeto visa reduzir o uso de papéis, tornando o processo de visualização de escalas mais sustentável e eficiente, eliminando a necessidade de quadros de avisos físicos para cada funcionário.
         </p>
     </div>
 </div>
